c2994d27a289d2e750e4c97077aa6b2485c8339a
Tatiana Cardona <clleidytatina@gmail.com>
2023-08-16 17:18:37 -0500

    login in nft and bid

--- src/api/bid/index.ts
+++ src/api/bid/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getAllBidHandler, getCreateBidHandler } from "./bid.controller";
-
+import { isAuthenticated } from "../../auth/auth.controller";
+import { hasRole } from "../../auth/auth.controller";
 const router = Router();
 
-router.get('/:autionId', getAllBidHandler);
-router.post('/',getCreateBidHandler)
+router.get('/:autionId',isAuthenticated, getAllBidHandler);
+router.post('/',isAuthenticated,hasRole('USER'),getCreateBidHandler)
 
 export default router;
--- src/api/like/index.ts
+++ src/api/like/index.ts
@@ -6,9 +6,10 @@ import {
   getDeleteLikeHandler
 } from "./like.controller";
 import { isAuthenticated } from "../../auth/auth.controller";
+import { hasRole } from "../../auth/auth.controller";
 const router =  Router()
 
 router.get('/:nftId', getAllLikeHandler)
-router.post('/:nftId', isAuthenticated, getCreateLikeHandler)
-router.delete('/:nftId', isAuthenticated, getDeleteLikeHandler)
+router.post('/:nftId', isAuthenticated, hasRole('USER'), getCreateLikeHandler)
+router.delete('/:nftId', isAuthenticated, hasRole('USER'),getDeleteLikeHandler)
 export default router

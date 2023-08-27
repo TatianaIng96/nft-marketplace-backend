159c06e116b2a1df254546cab97421097d79c97a
Camilo S <68169750+Camilo-Suarez98@users.noreply.github.com>
2023-08-27 18:53:55 -0500

    Fix post request (#14)

--- src/api/nftOwner/index.ts
+++ src/api/nftOwner/index.ts
@@ -12,9 +12,9 @@ import { isAuthenticated, hasRole } from "../../auth/auth.controller";
 const router = Router()
 
 router.get('/', isAuthenticated, hasRole('ADMIN'), getAllNftOwnersHandler);
+router.post('/', isAuthenticated, createNftOwnerHandler);
 
 router.get('/:id', getNftOwnerByIdHandler);
-router.post('/:id', isAuthenticated, createNftOwnerHandler);
 router.put('/:id', isAuthenticated, updateNftOwnerHandler);
 
 export default router;

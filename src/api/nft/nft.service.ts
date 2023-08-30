51f5ad842bf690303633e54ec7a8cc6225d8eb23
Juan Camilo Clement Santacruz <132512082+JuanCamiloClement@users.noreply.github.com>
2023-08-30 11:15:09 -0500

    Fix/cors image error (#18)
    * add nftOwner allnft
    
    * add join in nft service
    
    * adds all origins to cors
    
    * adds app.all function
    
    * adds allow origin header in app.all function
    
    ---------
    
    Co-authored-by: Tatiana Cardona <clleidytatina@gmail.com>

--- src/api/nft/nft.service.ts
+++ src/api/nft/nft.service.ts
@@ -27,6 +27,20 @@ export const getAllNft = async () => {
                         }
                     }
                 }
+            },
+            nftOwner: {
+                where: {
+                    isCurrentOwner: true,
+                },
+                select: {
+                    id:true,
+                    user: {
+                        select: {
+                            id: true,
+                            firstName: true
+                        }
+                    }
+                }
             }
         }
     })
@@ -46,9 +60,11 @@ export const getNftById = async (id: string) => {
             },
             auction: {
                 orderBy: {
-                    createdAt: 'asc' // Ordenar subastas por createdAt en orden descendente
+                    createdAt: 'desc' // Ordenar subastas por createdAt en orden descendente
                 },
-                take: 1,
+                where: {
+                    nftId: id // Filtrar subastas por nftOwnerId igual a id del NFT
+                },
                 select: {
                     id: true,
                     finishDate: true,
@@ -68,7 +84,6 @@ export const getNftById = async (id: string) => {
                     isCurrentOwner: true,
                 },
                 select: {
-                    id:true,
                     user: {
                         select: {
                             id: true,

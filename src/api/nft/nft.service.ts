4a34bf8d077e25aa289bf4228b7b23b7fdd20b3e
Tatiana Cardona <clleidytatina@gmail.com>
2023-08-29 19:23:54 -0500

    add nftOwner allnft

--- src/api/nft/nft.service.ts
+++ src/api/nft/nft.service.ts
@@ -27,6 +27,20 @@ export const getAllNft = async () => {
                         }
                     }
                 }
+            },
+            nftOwner: {
+                where: {
+                    isCurrentOwner: true,
+                },
+                select: {
+                    id:true,
+                    user: {
+                        select: {
+                            id: true,
+                            firstName: true
+                        }
+                    }
+                }
             }
         }
     })
@@ -68,7 +82,6 @@ export const getNftById = async (id: string) => {
                     isCurrentOwner: true,
                 },
                 select: {
-                    id:true,
                     user: {
                         select: {
                             id: true,

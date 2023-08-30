549c1da472b48847fd53c04212de131c61c07ff3
Juan Camilo Clement Santacruz <juancamiloclement@gmail.com>
2023-08-30 12:56:24 -0500

    tries first add headers function

--- src/config/express.ts
+++ src/config/express.ts
@@ -2,14 +2,8 @@ import express, { Application } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 
-const allowedOrigins = ['*'];
-
-const options = {
-    origin: allowedOrigins,
-}
-
 const configExpress = (app: Application) => {
-    app.use(cors(options));
+    app.use(cors());
     app.use(morgan('dev'));
     app.use(express.json());
 }

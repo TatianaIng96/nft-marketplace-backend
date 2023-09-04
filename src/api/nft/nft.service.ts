c3197320e8bf6082c1505b3c6b57ba64a015186d
Tatiana Cardona <104793222+TatianaIng96@users.noreply.github.com>
2023-09-04 11:02:26 -0500

    Feature/filter (#20)
    * add nftOwner allnft
    
    * add join in nft service
    
    * adds all origins to cors
    
    * adds * to cors origin
    
    * changes + for * in cors
    
    * removes response headers configuration
    
    * only * in cors
    
    * removes * from cors
    
    * organizes options for cors
    
    * tries first add headers function
    
    * moves first function to routes
    
    * tries second add headers function
    
    * add object in cors
    
    * querys filter in nft
    
    ---------
    
    Co-authored-by: Juan Camilo Clement Santacruz <juancamiloclement@gmail.com>

--- src/api/nft/nft.service.ts
+++ src/api/nft/nft.service.ts
@@ -2,12 +2,13 @@ import { PrismaClient } from "@prisma/client";
 
 import { Nft } from "./nft.types";
 import { User } from '../user/user.types';
+import { Collection } from '../collection/collection.types';
 
 const prisma = new PrismaClient()
 const now = new Date(); // Obtiene la fecha y hora actual
 
-export const getAllNft = async () => {
-    const nft = await prisma.nft.findMany({
+export const getAllNft = async (likes?: string, categoryId?:number, collectionId?: number, price?: number) => {
+    const nftFilters:any = {
         include: {
             like: {
                 select: {
@@ -41,10 +42,35 @@ export const getAllNft = async () => {
                         }
                     }
                 }
+            },
+        },
+    
+    };
+
+    if (likes !== undefined) {
+        nftFilters.orderBy = { like: { _count: likes === "most" ? "desc" : "asc" } };
+    }
+
+    if (categoryId !== undefined) {
+        nftFilters.where = { ...nftFilters.where, categoryId };
+    }
+
+    if (collectionId !== undefined) {
+        nftFilters.where = { ...nftFilters.where, collectionId };
+    }
+
+    if (price !== undefined) {
+        nftFilters.where = {
+            ...nftFilters.where,
+            price: {
+                gt: 0,
+                lt: price
             }
-        }
-    })
-    return nft
+        };
+    }
+
+    const nfts = await prisma.nft.findMany(nftFilters);
+    return nfts
 }
 
 export const getNftById = async (id: string) => {
@@ -128,4 +154,46 @@ export const deleteNft = async (id: string) => {
     });
 
     return nft;
+}
+
+export const filterCategory = (category: string | undefined) => {
+
+    const categories:{ [key: string]: number }= { 
+        "art": 2, 
+        "music": 4, 
+        "video": 5, 
+        "collectionable": 6,
+    }
+ 
+    let categoryId :number | undefined;
+
+    for (let key in categories) {
+        const value= categories[key];
+        if (key === category) {
+        categoryId = value;
+        break; // Si encontramos la clave "art", salimos del bucle
+        }
+    }
+
+    return categoryId
+}
+export const filterCollection = (collection: string | undefined) => {
+    const collections:{ [key: string]: number }= { 
+        "art-decco": 2,
+        "bored-ape-yacht-club": 4, 
+        "mutant-ape-yacht-club": 5, 
+        "art-blocks-factory": 6
+    }
+ 
+    let collectionId :number | undefined;
+
+    for (let key in collections) {
+        const value= collections[key];
+        if (key === collection) {
+        collectionId = value;
+        break; // Si encontramos la clave "art", salimos del bucle
+        }
+    }
+
+    return collectionId
 }
--- src/config/express.ts
+++ src/config/express.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import morgan from 'morgan';
 
 const configExpress = (app: Application) => {
-    app.use(cors());
+    app.use(cors({ credentials:true }));
     app.use(morgan('dev'));
     app.use(express.json());
 }

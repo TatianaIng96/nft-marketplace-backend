609ba32f50f78047f198ad4db09eeaacfdaa6346
Juan Camilo Clement Santacruz <juancamiloclement@gmail.com>
2023-09-07 13:31:38 -0500

    adds allow all origins to cors

--- src/config/express.ts
+++ src/config/express.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import morgan from 'morgan';
 
 const configExpress = (app: Application) => {
-    app.use(cors({ credentials:true }));
+    app.use(cors({ origin: "*" }));
     app.use(morgan('dev'));
     app.use(express.json());
 }

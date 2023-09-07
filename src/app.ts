55966e9fa7bdaed08da6a36830347e2ba3026252
Juan Camilo Clement Santacruz <juancamiloclement@gmail.com>
2023-09-07 14:00:08 -0500

    adds allow all origins header to response

--- src/app.ts
+++ src/app.ts
@@ -10,11 +10,8 @@ configExpress(app);
 
 routes(app);
 
-app.use((_, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+app.all('/', (_, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
     next();
 });
 

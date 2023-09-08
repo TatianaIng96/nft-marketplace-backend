d14690b84aec399514eaef7b9715d6a864855f0f
Juan Camilo Clement Santacruz <juancamiloclement@gmail.com>
2023-09-08 14:11:24 -0500

    returns to using cors npm

--- src/app.ts
+++ src/app.ts
@@ -6,32 +6,7 @@ import routes from "./routes";
 const app = express();
 const port = process.env.PORT || 8080;
 
-// app.use((req, _, next) => {
-//     console.log('REQUEST HEADERS BEFORE EVERYTHING', req.headers)
-//     next()
-// })
-
-app.use(function (req, res, next) {
-    const allowedOrigins = ['https://nft-marketplace-frontend-afh57hu61-tatianaing96.vercel.app/edit-profile-image'];
-    const origin = req.headers.origin;
-    if (!origin) {
-        return res.json({ message: 'No origin header found' });
-    }
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-    next();
-});
-
 configExpress(app);
-
-// app.use((req, _, next) => {
-//     console.log('REQUEST HEADERS BEFORE ROUTES', req.headers)
-//     next()
-// })
 
 routes(app);
 
--- src/config/express.ts
+++ src/config/express.ts
@@ -1,9 +1,9 @@
 import express, { Application } from 'express';
-// import cors from 'cors';
+import cors from 'cors';
 import morgan from 'morgan';
 
 const configExpress = (app: Application) => {
-    // app.use(cors());
+    app.use(cors());
     app.use(morgan('dev'));
     app.use(express.json());
 }

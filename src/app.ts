aceffeba461a05860dedd09cb03567b5e3e40c13
Juan Camilo Clement Santacruz <juancamiloclement@gmail.com>
2023-09-08 12:34:59 -0500

    configures cors manually

--- src/app.ts
+++ src/app.ts
@@ -6,22 +6,37 @@ import routes from "./routes";
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use((req, _, next) => {
-    console.log('REQUEST HEADERS BEFORE EVERYTHING', req.headers)
-    next()
-})
+// app.use((req, _, next) => {
+//     console.log('REQUEST HEADERS BEFORE EVERYTHING', req.headers)
+//     next()
+// })
+
+app.use((_, res, next) => {
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "*"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Private-Network", "true");
+    res.setHeader("Access-Control-Max-Age", 7200);
+
+    next();
+});
 
 configExpress(app);
 
-// app.use((_, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', 'https://nft-marketplace-frontend-9z7xtntur-tatianaing96.vercel.app/*');
-//     next();
-// });
-
-app.use((req, _, next) => {
-    console.log('REQUEST HEADERS BEFORE ROUTES', req.headers)
-    next()
-})
+// app.use((req, _, next) => {
+//     console.log('REQUEST HEADERS BEFORE ROUTES', req.headers)
+//     next()
+// })
 
 routes(app);
 
